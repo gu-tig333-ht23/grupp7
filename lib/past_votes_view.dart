a61301653500c57bbd2311fe2b30cdc9aedfac60
Ceionx <102590226+Ceionx@users.noreply.github.com>
2023-10-12 11:50:20 +0200

    Fixar voteringsvyn igen

--- lib/past_votes_view.dart
+++ lib/past_votes_view.dart
@@ -1,38 +1,25 @@
-
 import 'package:flutter/material.dart';
+import 'package:template/info_view.dart';
 import './theme.dart';
 
 List<Voteringar> voteringar = [
-  Voteringar('2022/23 AU10', 'En fortsatt stärkt arbetslöshetsförsäkring',
-      '2023-10-07', true),
+  Voteringar('2022/23 AU10', 'En fortsatt stärkt arbetslöshetsförsäkring', '2023-10-07', true),
   Voteringar('2022/23 AU5', 'Arbetsrätt', '2023-09-28', true),
   Voteringar('2022/23 AU5', 'Integration', '2023-09-20', false),
-  Voteringar(
-      '2022/23 CU7', 'Ersättningsrätt och insolvensrätt', '2023-09-07', false),
-  Voteringar(
-      '2021/22:FöU3',
-      'Riksrevisionens rapport om projektbidrag från anslag 2:4 Krisberedskap',
-      '2023-08-23',
-      true),
+  Voteringar('2022/23 CU7', 'Ersättningsrätt och insolvensrätt', '2023-09-07', false),
+  Voteringar('2021/22:FöU3', 'Riksrevisionens rapport om projektbidrag från anslag 2:4 Krisberedskap', '2023-08-23', true),
 ];
-
 
 class Voteringar extends StatelessWidget { // Can be used for UPCOMING votes/voteringar
   final String identification;
   final String title;
   final String decisionDate;
-  final bool
-      isAccepted; // To display whether the proposition was accepted or rejected
+  final bool isAccepted; // To display whether the proposition was accepted or rejected
 
-  Voteringar(
-      this.identification, this.title, this.decisionDate, this.isAccepted,
-      {super.key});
+  Voteringar(this.identification, this.title, this.decisionDate, this.isAccepted, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    final AppColors appColors = AppColors(); // Remove after theme.dart is updated
-
     return Padding(
       padding: EdgeInsets.only(left: 25, right: 25, top: 0, bottom: 0),
       child: Column(
@@ -40,7 +27,7 @@ class Voteringar extends StatelessWidget { // Can be used for UPCOMING votes/vot
           Container(
             padding: EdgeInsets.only(top: 5, bottom: 10, left: 10),
             decoration: BoxDecoration(
-              border: Border.all(style: BorderStyle.solid, color: appColors.yellow, width: 1.8),
+              border: Border.all(style: BorderStyle.solid, color: AppColors.yellow, width: 1.8),
               boxShadow: [
                 BoxShadow(
                   offset: Offset(0, 3),
@@ -48,7 +35,7 @@ class Voteringar extends StatelessWidget { // Can be used for UPCOMING votes/vot
                   color: Colors.black.withOpacity(0.4),
                 ),
               ], 
-              color: appColors.primaryBlue,
+              color: AppColors.primaryBlue,
               borderRadius: BorderRadius.circular(20),
               ),
             child: Row(
@@ -67,7 +54,7 @@ class Voteringar extends StatelessWidget { // Can be used for UPCOMING votes/vot
                   padding: const EdgeInsets.only(right: 5, left: 5),
                   child: Icon(
                     isAccepted ? Icons.check : Icons.close, // Placeholder for icons, update later
-                    color: isAccepted ? appColors.green : appColors.red,
+                    color: isAccepted ? AppColors.green : AppColors.red,
                     ),
                 )
               ],
@@ -83,20 +70,14 @@ final List<bool> selectedVotering = <bool>[true, false];
 
 class VoteringsVy extends StatefulWidget {
   const VoteringsVy({super.key});
-
+  
   @override
   State<VoteringsVy> createState() => _VoteringsVyState();
 }
 
 class _VoteringsVyState extends State<VoteringsVy> {
   @override
-  State<VoteringsVy> createState() => _VoteringsVyState();
-}
-
-class _VoteringsVyState extends State<VoteringsVy> {
-  @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context) {    
     const List<Widget> voteringsDisplay = <Widget>[
       Text('Genomförda'),
       Text('Kommande'),
@@ -104,9 +85,8 @@ class _VoteringsVyState extends State<VoteringsVy> {
 
     return Scaffold(
       appBar: AppBar(
-
         centerTitle: true,
-        backgroundColor: appColors.lightGrey,
+        backgroundColor: AppColors.lightGrey,
         title: Text('Voteringar', style: AppFonts.header),
         actions: [
           Padding(
@@ -114,7 +94,7 @@ class _VoteringsVyState extends State<VoteringsVy> {
             child: FloatingActionButton(
               shape: CircleBorder(),
               tooltip: 'Om appen',
-              backgroundColor: appColors.lightGrey,
+              backgroundColor: AppColors.lightGrey,
               onPressed: () {
                 aboutAppAlert(context);
               },
@@ -125,7 +105,7 @@ class _VoteringsVyState extends State<VoteringsVy> {
       ),
       body: Container(
         height: MediaQuery.of(context).copyWith().size.height,
-        color: appColors.backgroundColor,
+        color: AppColors.backgroundColor,
         child: Column(
           children: [
             Padding(
@@ -140,7 +120,7 @@ class _VoteringsVyState extends State<VoteringsVy> {
                     onTap: () { 
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => VoteringsVy()),
+                        MaterialPageRoute(builder: (context) => InfoView()),
                       );
                     },
                     child: Voteringar(
@@ -159,24 +139,21 @@ class _VoteringsVyState extends State<VoteringsVy> {
                 direction: Axis.horizontal,
                 onPressed: (int index) {
                   setState(() {
-
                   for (int i = 0; i < selectedVotering.length; i++) {
                       selectedVotering[i] = i == index;}
                       });
                 },
                 borderRadius: const BorderRadius.all(Radius.circular(8)),
-                selectedBorderColor: appColors.darkGrey,
-                selectedColor: appColors.lightGrey,
-                fillColor: appColors.mediumGrey,
-                color: appColors.black,
-
+                selectedBorderColor: AppColors.darkGrey,
+                selectedColor: AppColors.lightGrey,
+                fillColor: AppColors.mediumGrey,
+                color: AppColors.black,
                 constraints: const BoxConstraints(
                   minHeight: 40,
                   minWidth: 100,
                 ),
                 isSelected: selectedVotering,
                 children: voteringsDisplay,
-
               )
             )
           ]
@@ -185,7 +162,6 @@ class _VoteringsVyState extends State<VoteringsVy> {
     );
   }
 }
-
 
 Widget aboutAppAlert(context) {   // Pop-up window (About the app with reference to riksdag.se)
     Widget confirmButton = TextButton(child: Text('Ok'),
@@ -199,4 +175,3 @@ Widget aboutAppAlert(context) {   // Pop-up window (About the app with reference
     showDialog(context: context, builder: (BuildContext context){return alert;});
       return alert;
   }
-
