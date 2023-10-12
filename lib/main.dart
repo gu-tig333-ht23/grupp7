71e54205d88e9e9a029b31c63c32507ea2bdde3c
TIGStudent <131448346+TIGStudent@users.noreply.github.com>
2023-10-12 23:52:08 +0200

    förberätt main för hantering av flera provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:template/info_view.dart';
-import 'package:template/past_votes_view.dart';
+import 'package:provider/provider.dart';
+import 'ledarmot_vy/ledamot_vy.dart';
+import './provider/provider_ledamot.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProviderLedamot(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InfoView(),
+      title: 'API test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LedamotVy(),
     );
   }
 }
