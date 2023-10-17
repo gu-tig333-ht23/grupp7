f7ae9006ad6268b830f760185a283192d2d86d61
DoktorGlaze <98591989+DoktorGlaze@users.noreply.github.com>
2023-10-17 12:40:55 +0200

    små ui justeringar

--- lib/party_view.dart
+++ lib/party_view.dart
@@ -29,21 +29,21 @@ class PartyView extends StatelessWidget {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              selectedTheme.name, // Set the text dynamically
-              style: AppFonts.title.copyWith(
-                color: Colors.white, // Set text color
-              ),
-            ),
-            SizedBox(width: 8), // Adjust the spacing between text and image
             selectedTheme.assetImage.isNotEmpty
                 ? Image.asset(
                     selectedTheme.assetImage,
-                    width: 40, // Adjust the size as needed
-                    height: 40, // Adjust the size as needed
+                    width: 40,
+                    height: 40,
                     fit: BoxFit.cover,
                   )
-                : Container(), // If assetImage is empty, don't show an image
+                : Container(),
+            SizedBox(width: 15),
+            Text(
+              selectedTheme.name,
+              style: AppFonts.title.copyWith(
+                color: Colors.white,
+              ),
+            ),
           ],
         ),
         backgroundColor: selectedTheme.color,
@@ -53,118 +53,122 @@ class PartyView extends StatelessWidget {
       body: ListView(
         keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         children: [
-          Column(
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 60,
-                          width: 60,
-                          child: Image.asset(
-                              "assets/images/socialdemokraterna.png"),
-                        ),
-                        Text(
-                          "Socialdemokraterna",
-                          style: AppFonts.headerRed,
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // lägg till bild på partiledare här
-                        ClipOval(
-                          child: Image.network(
-                            'https://data.riksdagen.se/filarkiv/bilder/ledamot/dcc2ab7d-3fc1-4a28-b3c7-be1679c047b3_80.jpg',
-                            width: 60, // Adjust the size as needed
-                            height: 60, // Adjust the size as needed
-                            fit: BoxFit.cover,
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Column(
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      //Row(
+                      //  mainAxisAlignment: MainAxisAlignment.center,
+                      //  children: [
+                      //    SizedBox(
+                      //      height: 60,
+                      //      width: 60,
+                      //      child: Image.asset(
+                      //          "assets/images/socialdemokraterna.png"),
+                      //    ),
+                      //    Text(
+                      //      "Socialdemokraterna",
+                      //      style: AppFonts.headerRed,
+                      //    )
+                      //  ],
+                      //),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // lägg till bild på partiledare här
+                          ClipOval(
+                            child: Image.network(
+                              'https://data.riksdagen.se/filarkiv/bilder/ledamot/dcc2ab7d-3fc1-4a28-b3c7-be1679c047b3_80.jpg',
+                              width: 60, // Adjust the size as needed
+                              height: 60, // Adjust the size as needed
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
 
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Partiledare:",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text("Magdalena Andersson"),
-                            SizedBox(height: 16),
-                            RichText(
-                              text: TextSpan(
-                                text: partyWebPage,
-                                style: TextStyle(
-                                  color: Colors.blue, // Set the hyperlink color
-                                  decoration: TextDecoration.underline,
-                                ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    // Implement the action you want when the link is tapped
-                                    launchUrl(partyUrl);
-                                  },
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Partiledare:",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(fontWeight: FontWeight.bold),
                               ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Divider(
-                        thickness: 1,
-                        color: Colors.black,
+                              Text("Magdalena Andersson"),
+                              SizedBox(height: 16),
+                              RichText(
+                                text: TextSpan(
+                                  text: partyWebPage,
+                                  style: TextStyle(
+                                    color:
+                                        Colors.blue, // Set the hyperlink color
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      // Implement the action you want when the link is tapped
+                                      launchUrl(partyUrl);
+                                    },
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
                       ),
-                    ),
-                    Text(
-                      "Partiets reslutat i frågan: $proposal.",
-                      textAlign: TextAlign.center,
-                    ),
-                    LedamotVyStatBar(
-                        theList: context.watch<ProviderLedamot>().theList),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Divider(
-                        thickness: 1,
-                        color: Colors.black,
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextField(
-                        controller: _textEditingController,
-                        decoration: InputDecoration(
-                            labelText: "Sök ledamot",
-                            filled: true,
-                            fillColor: Colors.grey[200], // Background color
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.circular(10.0), // Rounded edges
-                            ),
-                            suffixIcon: Icon(Icons.search),
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 4) // Search icon to the right
-                            ),
+                      Text(
+                        "Partiets reslutat i frågan: $proposal.",
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                    ListView.builder(
-                      itemBuilder: (context, index) {
-                        return LedamotItem(ledamotList[index]);
-                      },
-                      itemCount: ledamotList.length,
-                      shrinkWrap: true,
-                      physics: ScrollPhysics(),
-                    ),
-                  ],
+                      LedamotVyStatBar(
+                          theList: context.watch<ProviderLedamot>().theList),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                              labelText: "Sök ledamot",
+                              filled: true,
+                              fillColor: Colors.grey[200], // Background color
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(
+                                    10.0), // Rounded edges
+                              ),
+                              suffixIcon: Icon(Icons.search),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 4) // Search icon to the right
+                              ),
+                        ),
+                      ),
+                      ListView.builder(
+                        itemBuilder: (context, index) {
+                          return LedamotItem(ledamotList[index]);
+                        },
+                        itemCount: ledamotList.length,
+                        shrinkWrap: true,
+                        physics: ScrollPhysics(),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              //LedamotItem()
-            ],
+                //LedamotItem()
+              ],
+            ),
           ),
         ],
       ),
