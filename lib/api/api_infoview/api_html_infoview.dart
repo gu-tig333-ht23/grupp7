dfd31610105bd917b469fc69fd322d049a7c43d3
Ceionx <102590226+Ceionx@users.noreply.github.com>
2023-10-23 14:57:29 +0200

    Updated Home+Calendar-View

--- lib/api/api_infoview/api_html_infoview.dart
+++ lib/api/api_infoview/api_html_infoview.dart
@@ -12,34 +12,38 @@ Future<String> getSummary(beteckning) async {
     if (response.statusCode == 200) {
       dom.Document html = dom.Document.html(response.body);
 
-      html.querySelectorAll('p').forEach((element) {
-        if (element.classes.contains(
-                'Sammanfattning') || // dessa är lite oklara för tillfället eftersom att HTML-koden ser olika ut beroende på vilken fråga vi är inne på.
-            element.classes.contains('R1')) {
-          isParsing = true;
-        } else if (element.classes.contains('Innehllsfrteckning')) {
-          isParsing = false;
-        } else if (isParsing) {
-          List<dom.Element> spans =
-              element.querySelectorAll('span'); // Hämtar alla span-objekt
-          final text = spans
-              .where((spanElement) =>
-                  spanElement.attributes['style'] ==
-                  null) // Filtrerar ut <span style> objekt som fuckar upp
-              .map((spanElement) => spanElement.innerHtml.trim())
-              .join(' ');
+      html.querySelectorAll('p').forEach(
+        (element) {
+          if (element.classes.contains(
+                  'Sammanfattning') || // dessa är lite oklara för tillfället eftersom att HTML-koden ser olika ut beroende på vilken fråga vi är inne på.
+              element.classes.contains('R1')) {
+            isParsing = true;
+          } else if (element.classes.contains('Innehllsfrteckning')) {
+            isParsing = false;
+          } else if (isParsing) {
+            List<dom.Element> spans =
+                element.querySelectorAll('span'); // Hämtar alla span-objekt
+            final text = spans
+                .where(
+                  (spanElement) =>
+                      spanElement.attributes['style'] == null &&
+                      !spanElement.innerHtml.contains('&nbsp;'),
+                ) // Filtrerar ut <span style> objekt och de som innehåller enbart '&nbsp;'
+                .map((spanElement) => spanElement.innerHtml.trim())
+                .join(' ');
 
-          if (spans.length == 1) {
-            if (text.isNotEmpty) {
-              summary += '$text\n';
-            }
-          } else {
-            if (text != '&nbsp;' || text != '&shy;') {
-              summary += '$text\n';
+            if (spans.length == 1) {
+              if (text.isNotEmpty) {
+                summary += '$text\n';
+              }
+            } else {
+              if (text != '&nbsp;' || text != '&shy;') {
+                summary += '$text\n';
+              }
             }
           }
-        }
-      });
+        },
+      );
     } else {
       print('Failed to load data: ${response.statusCode}');
     }
